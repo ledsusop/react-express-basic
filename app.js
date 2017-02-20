2e51b72f7f7215d51df57798f27e235d40f7a22a
ledsusop <ledsusop@gmail.com>
2017-02-20 05:42:01 -0800

    add app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bunyan = require('bunyan');
+
+var app = express();
+var log = bunyan.createLogger({name:'react-express-basic'});
+
+app.get('/',function(req,res){
+    res.send('Nothing here');
+})
+
+app.get('/api',function(req,res){
+    res.send('APIs here');
+})
+
+app.get('/lp',function(req,res){
+    res.send('LPs here');
+})
+
+app.listen(3000,function(){
+    log.info(' APPLICATION STARTED AND LISTENING AT PORT 3000')
+})
